611d216841b41971e3305fea8c5284bcfc7f4ebf
992903713 <992903713@qq.com>
2018-10-05 15:12:11 +0800

    18-10-5 combined Charts

---
+++ Cactus/Classes/Main/HomePage/LessonManagement/DataAnalyses/Controller/CADataAnalysesViewController.h
@@ -0,0 +1,14 @@
+//
+//  CADataAnalysesViewController.h
+//  Cactus
+//
+//  Created by 钟奇龙 on 2018/9/22.
+//  Copyright © 2018年 钟奇龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CAClass.h"
+@interface CADataAnalysesViewController : UIViewController
+@property (nonatomic,strong) CAClass *lessonClass;
+
+@end
---
+++ Cactus/Classes/Others/Lib/UIView+CAExtension.h
@@ -0,0 +1,22 @@
+//
+//  UIView+CAExtension.h
+//  Cactus
+//
+//  Created by 钟奇龙 on 2018/10/5.
+//  Copyright © 2018 钟奇龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (CAExtension)
+
+- (CGFloat) x;
+- (CGFloat) y;
+- (CGFloat) width;
+- (CGFloat) height;
+
+@end
+
+NS_ASSUME_NONNULL_END
